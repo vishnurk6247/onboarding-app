8fe844631642476a5305c0ad91dd069769dff0b9
vishnu r kumar <rkumar.vishnu28@gmail.com>
2022-07-11 14:31:51 +0530

    Change ui for login

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
 import "./styles/global-styles.css";
 import "./styles/components.css";
-import Onboarding from "./pages/onboarding";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// import Onboarding from "./pages/onboarding";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useEffect } from "react";
 
 function App() {
-  useEffect(() => {
+  const invokeDeeplink = () => {
     const req_nonce = Math.floor(Math.random() * 899999 + 100000000);
     const BASE_PATH =
       "truecallersdk://truesdk/web_verify?" +
       `requestNonce=${req_nonce}` +
+      `&type=btmsheet` +
       `&partnerKey=SXF5f9162faafe54c42b889a1211dde9f3567` +
       `&partnerName=TrueShop&lang=en&title=login`;
     window.location.href = BASE_PATH;
+  };
+  useEffect(() => {
+    invokeDeeplink();
   }, []);
+
   return (
     <>
       <div className="app-container">
         <BrowserRouter>
           <Routes>
             <Route
-              exact
               path="/"
-              element={<Navigate to="/onboarding/profile" />}
+              element={<button onClick={invokeDeeplink}>Login</button>}
             />
-            <Route path="/onboarding/*" element={<Onboarding />} />
+            {/* <Route path="/onboarding/*" element={<Onboarding />} /> */}
           </Routes>
         </BrowserRouter>
       </div>
